7166a6bae3172bf342b44b86f9fa682fa1078316
Emil <neanderemil@gmail.com>
2021-01-26 20:40:39 +0100

    Testar att pusha, lagt in expenseManager

---
+++ Managers/ExpenseManager.cs
@@ -0,0 +1,30 @@
+﻿using BudgetImpossibleManager.Data;
+using BudgetImpossibleManager.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetImpossibleManager.Managers
+{
+    class ExpenseManager
+    {
+        public void AddExpense(Expense expense)
+        {
+            using (var db = new BIMContext())
+            {
+                db.Expenses.Add(expense);
+                db.SaveChanges();
+            }
+        }
+        public Expense GetExpenseById(int id)
+        {
+            using (var db = new BIMContext())
+            {
+                var expense = db.Expenses.Find(id);
+                return expense;
+            }
+        }
+    }
+}
